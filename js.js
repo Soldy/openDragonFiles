e63548021f69009fc6e90722c7119429c34e318e
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-18 23:56:33 +0000

    js interface
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ js.js
@@ -0,0 +1,46 @@
+
+'use strict'
+
+
+exports.jsBase = function(){
+    /*
+     * @public 
+     * @return {string}
+     *
+     */
+    this.get=function(files){};
+    /*
+     * @param {function}
+     * @public 
+     * @return {boolean}
+     *
+     */
+    this.addProcesseer=function(func){};
+    /*
+     * @private 
+     * @var {string}
+     */
+    let container = "";
+    /*
+     * @private 
+     * @var {dictonary}
+     */
+    let cache = {};
+    /*
+     * @private 
+     */
+    let update = function(){};
+    /*
+     * @param {string}
+     * @private 
+     * @return {boolean}
+     */
+    let read = function(file){};
+    /*
+     * @private 
+     */
+    let processer = function(){};
+
+
+}
+
