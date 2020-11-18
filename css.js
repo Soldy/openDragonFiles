79496cb82c70049f52133827631f7daa90f75ff3
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-18 23:56:22 +0000

    css interface
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ css.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+exports.cssBase = function(){
+    /*
+     * @public 
+     * @return {string}
+     *
+     */
+    this.get=function(files){};
+    /*
+     * @param {function}
+     * @public 
+     * @return {boolean}
+     *
+     */
+    this.addProcesseer=function(func){};
+    /*
+     * @private 
+     * @var {string}
+     */
+    let container = "";
+    /*
+     * @private 
+     * @var {dictonary}
+     */
+    let cache = {};
+    /*
+     * @private 
+     */
+    let update = function(){};
+    /*
+     * @param {string}
+     * @private 
+     * @return {boolean}
+     */
+    let read = function(file){};
+    /*
+     * @private 
+     */
+    let processer = function(){};
+
+
+}
+
