cbef92d150fb7fc044549ebd2644ccb5fa212aa9
Soldy <4786022+Soldy@users.noreply.github.com>
2020-11-21 21:24:18 +0000

    html file
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ html.js
@@ -0,0 +1,48 @@
+'use strict'
+
+
+exports.htmlBase = function(parentIn){
+    /*
+     * @public 
+     * @return {string}
+     *
+     */
+    this.get=function(files){};
+    /*
+     * @param {function}
+     * @public 
+     * @return {boolean}
+     *
+     */
+    this.addProcesseer=function(func){};
+    /*
+     * @private 
+     * @var {object}
+     */
+    const openDragonFiles = parentIn;
+    /*
+     * @private 
+     * @var {string}
+     */
+    let container = "";
+    /*
+     * @private 
+     * @var {dictonary}
+     */
+    let cache = {};
+    /*
+     * @private 
+     */
+    let update = function(){};
+    /*
+     * @param {string}
+     * @private 
+     * @return {boolean}
+     */
+    let read = function(file){};
+    /*
+     * @private 
+     */
+    let processer = function(){};
+
+}
